3216d0c32adf1744ea9f4aefedaca5979a672d84
Julian Eng <julian3ng@gmail.com>
2017-08-04 23:40:18 -0700

    Hit records done

--- ocaml/_build/raytracer.ml
+++ ocaml/_build/raytracer.ml
@@ -46,28 +46,64 @@ module Ray = struct
     print_vec r.origin; print_vec r.direction
 end
 
+module HitRecord = struct
+  open Vec3
+  type hit_record' = {t_hit:float; p_hit:vec3; normal: vec3}
+  type hit_record = hit_record' option
+  let create_hit_record t p n = Some {t_hit=t; p_hit=p; normal=n}
+end
 
 module Sphere = struct
   open Vec3
   open Ray
+  open HitRecord
   type sphere = {center:vec3; radius:float}
   let create_sphere (c:vec3) (r:float) = {center=c; radius=r}
-  let hit_sphere (s:sphere) (r:ray) =
+  let hit_sphere (s:sphere) (r:ray) (t_min:float) (t_max:float)=
     let oc = sub r.origin s.center in
     let a = dot r.direction r.direction in
     let b = 2. *. (dot oc r.direction) in
     let c = (dot oc oc) -. (s.radius *. s.radius) in
     let discriminant = b*.b -. 4.*.a*.c in
-    if discriminant > 0.
-    then ((-.b) -. (sqrt discriminant)) /. (2. *. a)
-    else -1.
+    let first_root = ((-.b) -. (sqrt discriminant)) /. (2. *. a) in
+    let second_root = ((-.b) +. (sqrt discriminant)) /. (2. *. a) in
+    if discriminant > 0. then
+      if (first_root > t_min) && (first_root < t_max) then
+        let p = point_at_t r first_root in
+        create_hit_record first_root p (divf (sub p s.center) s.radius)
+      else
+        if (second_root > t_min) && (second_root < t_max) then
+          let p = point_at_t r first_root in
+          create_hit_record first_root p (divf (sub p s.center) s.radius)
+        else
+          None
+    else
+      None
 end
 
-
+module Collideable = struct
+  open Sphere
+  open Ray
+  type collideable = Sphere of sphere | Foo 
+  let hit (c:collideable) (r:ray) =
+    match c with
+    | Sphere s -> hit_sphere s r
+    | _ -> failwith "Not yet implemented"
+end
+(*
+module World = struct
+  open Ray
+  open Collideable
+  type world = collideable list
+  let hit (w:world) =
+end
+ *)
 module Main = struct
   open Vec3
   open Ray
   open Sphere
+  open Collideable
+  open HitRecord
 
   let nx = 200
   let ny = 100
@@ -76,33 +112,40 @@ module Main = struct
   let horizontal = create_vec3 4. 0. 0.
   let vertical = create_vec3 0. 2. 0.
 
-  let gen_ray i = 
-    let u = (float (i % nx)) /. (float nx) in
-    let v = (float (i / nx)) /. (float ny) in
+  let gen_ray ?rx ?ry i =
+    let dx = match rx with | None -> 0.0 | Some x -> x in
+    let dy = match ry with | None -> 0.0 | Some y -> y in
+    let u = ((float (i % nx)) +. dx) /. (float nx) in
+    let v = ((float (i / nx)) +. dy) /. (float ny) in
     create_ray (create_vec3 0. 0. 0.)
                (sum [llc ; mulf horizontal u; mulf vertical v])
-    
 
   let bg1 = create_vec3 0. 0. 0.
   let bg2 = create_vec3 0. 1. 0.
   let s = create_sphere (create_vec3 0. 0. (-1.)) 0.5
+  let world = Sphere s
         
   let color r =
-    let t = hit_sphere s r in
-    if t > 0.
-    then
-      let n = normalize (sub (point_at_t r t) s.center) in
-      mulf (addf n 1.) 0.5
-    else
-      let unit_direction = normalize r.direction in
-      let scaled_y = 0.5 *. (unit_direction.y +. 1.) in
-      lerp bg1 scaled_y bg2
+    let record = hit world r 0.001 1000.0 in
+    match record with
+    | None -> let unit_direction = normalize r.direction in
+              let scaled_y = 0.5 *. (unit_direction.y +. 1.) in
+              lerp bg1 scaled_y bg2
+    | Some {t_hit=_; p_hit=_; normal=n} -> 
+       mulf (addf n 1.) 0.5
 
+  let gen_sample i =
+    let rec aux j acc =
+      match j with
+      | 0 -> acc
+      | _ -> aux (j-1) (((gen_ray ~rx:(Random.float 1.) ~ry:(Random.float 1.) i))::acc)
+    in aux ns []
+      
   let gen_row i =
     let rec aux j acc =
       match j with
       | (-1) -> acc
-      | _ -> aux (j-1) ((gen_ray ((i*nx) + j)) :: acc)
+      | _ -> aux (j-1) ((gen_sample ((i*nx) + j))::acc) (*((gen_ray ((i*nx) + j)) :: acc)*)
     in aux (nx-1) []
 
   let gen_grid =
@@ -112,10 +155,16 @@ module Main = struct
       | _ -> aux (i-1) ((gen_row i) :: acc)
     in aux (ny-1) []
 
+  let antialias samples =
+    let colors = List.map samples ~f:color in
+    let total = List.fold_left colors ~init:(create_vec3 0. 0. 0.) ~f:add in
+    divf total (float (List.length colors))
+              
+
   let rec print_row row =
     match row with
     | [] -> ()
-    | h::t -> color h |> (fun v -> mulf v 255.) |> print_vec_i ; printf " "; print_row t
+    | h::t -> antialias h |> (fun v -> mulf v 255.) |> print_vec_i ; printf " "; print_row t
 
   let rec print_grid grid =
     match grid with
